724d89d314f073facdb302cf4ae0cd0dce0727b5
Barak <barakc121@gmail.com>
2025-12-07 12:37:30 +0200

    start

---
+++ riddles/index.js
@@ -0,0 +1,7 @@
+import r1 from "./r1";
+import r2 from "./r2";
+import r3 from "./r3";
+import r4 from "./r4";
+import r5 from "./r5";
+const all_riddels = [r1, r2, r3, r4, r5];
+export default all_riddels;
---
+++ utils/createPlayer.js
@@ -0,0 +1,35 @@
+
+
+export function createPlayer(name){
+    const player={
+    name,
+    times: [] 
+    }
+    return player
+}
+
+export function addSolveTime(player, seconds){
+    player.times.push(seconds)
+}
+
+export function showStats(player){
+    let totalTime=0
+    for (let i = 0; i < player.times.length; i++) {
+         totalTime += player.times[i];
+    }
+    const averge=totalTime/player.times.length
+    
+    console.log(`total time ${totalTime} seconds `)
+    console.log(`averge per one${averge}`)
+}
+
+export function test(){
+const username="enter your number :"
+    const player=createPlayer(username)
+    addSolveTime(player,3)
+    addSolveTime(player,8)
+    addSolveTime(player,4)
+    showStats(player)
+
+}
+test()
